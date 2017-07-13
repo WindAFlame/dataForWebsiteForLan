cb174fd404fa439a54e55b697fe7a4e0b208e135
WindAFlame <dev.windaflame@gmail.com>
2017-07-13 16:37:11 +0200

    update ?

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,28 @@ app.controller('MainCtrl',function($scope){
     $scope.form.model = {};
     $scope.form.input = {};
     $scope.form.input.release = new Date();
+    $scope.form.input.download = {};
+    $scope.form.input.download.model = {};
+    $scope.form.input.download.list = [];
+    $scope.form.input.download.value = {};
     $scope.form.submit = function(){
         if ($scope.form.model.$valid){
             alert(JSON.stringify(formatResult($scope.form.input)));
         } else {
+        }
+    };
+    $scope.form.input.download.submit = function(){
+        if ($scope.form.input.download.model.$valid){
+            $scope.form.input.download.list.push({
+                'name': $scope.form.input.download.value.name,
+                'link': $scope.form.input.download.value.link,
+                'size': $scope.form.input.download.value.size,
+                'system': $scope.form.input.download.value.system
+            });
+            $scope.form.input.download.value.name = null;
+            $scope.form.input.download.value.link = null;
+            $scope.form.input.download.value.size = null;
+            $scope.form.input.download.value.system = null;
         }
     };
 });
